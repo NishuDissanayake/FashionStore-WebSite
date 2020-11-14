34675009634efc944b088d2f651eba3f60a5f517
NishuDissanayake <nishudissanayake5252@gmail.com>
2020-11-14 21:48:22 +0530

    Updated home sketch

---
+++ js/navigtion.js
@@ -0,0 +1,19 @@
+//for auto hide nav bar 
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function(){
+
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos){
+
+        document.getElementById("navigation").style.top = "0";
+
+    }
+    else{
+
+        document.getElementById("navigation").style.top = "-75px";
+    }
+
+    prevScrollpos = currentScrollPos;
+}
